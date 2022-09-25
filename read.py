8cfe981d6c07e33c98ccef5b772f18fb483af5a1
DWW <tina871879156@gmail.com>
2022-09-25 12:00:29 +0900

    calc avg length of reviews

--- read.py
+++ read.py
@@ -1,16 +1,23 @@
 data = []
 count = 0
+sum_len = 0
+avg_len = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		#if count % 1000 == 0:
+			#print(len(data))
 
-#print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print(data[0]) #印出第一筆
+for d in data:
+	sum_len += len(d)
+
+avg_len = sum_len / len(data)
+print('留言平均長度為', avg_len)
+#print(data[0]) #印出第一筆
 #cls cmd畫面清掉
 
-print('--------------')
-print(data[1])
+#print('--------------')
+#print(data[1])
